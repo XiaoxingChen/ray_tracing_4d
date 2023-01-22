1c345da01cb228f974990d3f0750933db8c9f483
XiaoxingChen <chen-xx@zju.edu.cn>
2023-01-22 23:54:42 +0800

    optimize: duck gltf rasterization.

--- app/rasterization_3d.cpp
+++ app/rasterization_3d.cpp
@@ -74,26 +74,29 @@ void fragmentShading(
         std::vector<float> sample_pts_data;
         sample_pts_data.reserve(size_t(extent(0) * extent(1) + 1));
         
-        for(size_t i = size_t(prim_aabb.min()(0)); i < prim_aabb.max()(0) + 1; i++)
+        for(float i = std::max(prim_aabb.min(0), 0.f); i < std::min<float>(prim_aabb.max(0) + 1, output_img.shape(0)); i++)
         {
-            for(size_t j = size_t(prim_aabb.min()(1)); j < prim_aabb.max()(1) + 1; j++)
+            for(float j = std::max((prim_aabb.min(1)), 0.f); j < std::min<float>(prim_aabb.max(1) + 1, output_img.shape(1)); j++)
             {
-                sample_pts_data.push_back(float(i));
-                sample_pts_data.push_back(float(j));
+                sample_pts_data.push_back(i);
+                sample_pts_data.push_back(j);
             }
         }
 
+        if(sample_pts_data.empty()) continue; //frustum culling
+
         // std::cout << "extent: " <<  sample_pts_data.size() << std::endl;
         Matrix<float> sample_pts(fixRow(2), std::move(sample_pts_data), COL);
-        auto local_inside = splx::arePointsInside(sample_pts, prim, float(1e-2));
+        auto local_inside = splx::arePointsInside(sample_pts, prim, float(5e-2));
         
         auto bary_coord = splx::barycentricCoordinate(sample_pts, prim);
         // Pixel px_color = bary_coord.T().matmul(colors); // todo
         auto texture_coord = prim_texture_coord.matmul(bary_coord);
 
-        // std::cout << mxm::to_string(texture_coord) << std::endl;
-
+        texture_coord *= Vector<float>{float(texture.shape(0)), float(texture.shape(1))};
+        
         auto colors = interp::bilinearUnitSquare(texture_coord, texture);
+
         Vector<float> px_z = bary_coord.T().matmul(vertex_z);
 
         // std::cout << mxm::to_string(color) << std::endl;
@@ -103,8 +106,6 @@ void fragmentShading(
             Vector<int> px_coord(sample_pts(Col(i)));
             if(!screen_aabb.contains(px_coord)(0)) continue;
             if(local_inside(i) == 0) continue;
-            
-            
             output_img(px_coord(0), px_coord(1)) = colors(i, 0);
             z_buffer(px_coord(0), px_coord(1)) = px_z(i);
         }
@@ -114,42 +115,64 @@ void fragmentShading(
 
 int main(int argc, char const *argv[])
 {
-    Camera<float, DIM> cam;
-    cam.setFocalLength({250, 250}).setResolution({240, 320}).setPrincipalOffset({125, 125});
+    tinygltf::Model model;
+    const size_t DIM = 3;
+    loadModel(model, "build/assets/Duck.gltf");
 
-    Rotation<float> rot = Rotation<float>::fromAxisAngle({1.f, 0,0}, M_PI * 0.2);
+    Camera<float, DIM> cam;
+    // cam.setFocalLength({250, 250}).setResolution({240, 320}).setPrincipalOffset({125, 125});
+    cam.setFocalLength({500, 500}).setResolution({480, 640}).setPrincipalOffset({240, 320});
+
+    Rotation<float> rot = Rotation<float>::fromAxisAngle({1.f,0,0}, M_PI * 0.);
 
     Shape output_reso{cam.resolution(0), cam.resolution(1)};
     Vector<size_t> ncube_resolution{5,5,0};
 
     
 
-    Matrix<Pixel> texture({2,2}, {Pixel::red(), Pixel::green(), Pixel::blue(), Pixel::white()});
+    // Matrix<Pixel> texture({2,2}, {Pixel::red(), Pixel::green(), Pixel::blue(), Pixel::white()});
+    Matrix<Pixel> texture(*loadMeshTexture(model));
+    // std::cout << "color: " << mxm::to_string(texture(25,425)) << std::endl;
+    // std::cout << to_string (interp::bilinearUnitSquare(Vector<float>{0.048828125, 0.810546875}, texture)) << std::endl;
+    // exit(0);
     
+    std::shared_ptr<Mat> vertex_buffer = loadMeshAttributes(model, 0, "POSITION");
+    // auto vertices_3d = generateNCubeVertices({100.f, 100.f, 1.f}, ncube_resolution);
 
-    auto vertices_3d = generateNCubeVertices({100.f, 100.f, 1.f}, ncube_resolution);
+    *vertex_buffer = rot.apply(*vertex_buffer);
 
-    vertices_3d = rot.apply(vertices_3d);
-
-    vertices_3d += Vector<float>{-50,-50,100};
-    Vector<float> vertex_z_buffer = vertexZBuffer(cam, vertices_3d);
+    *vertex_buffer += Vector<float>{0,-50,250};
+    Vector<float> vertex_z_buffer = vertexZBuffer(cam, *vertex_buffer);
 
 
-    Matrix<float> texture_coord_buffer = random::uniform<float>({2, vertices_3d.shape(1)});
+    // Matrix<float> texture_coord_buffer = random::uniform<float>({2, vertex_buffer->shape(1)});
+    Matrix<float> texture_coord_buffer(*loadMeshAttributes(model, 0, "TEXCOORD_0"));
 
-    auto indices = generateSquareTriangleIndices(ncube_resolution);
+    {
+        Matrix<float> tmp = texture_coord_buffer(Row(0));
+        texture_coord_buffer(Row(0)) = texture_coord_buffer(Row(1));
+        texture_coord_buffer(Row(1)) = tmp;
+    }
+    
+    // std::cout << mxm::to_string(texture_coord_buffer(Block({},{0,100})).T()) << std::endl;
+
+    // auto indices = generateSquareTriangleIndices(ncube_resolution);
+    auto vertex_index_buffer = std::make_shared<Matrix<size_t>>(std::move(loadMeshIndices(model, 0)));
+
+    // *vertex_index_buffer = (*vertex_index_buffer)(Block({},{0,10}));
+    // std::cout << mxm::to_string(indices.shape()) << std::endl;
 
     Matrix<Pixel> img_out(output_reso);
     Matrix<float> z_buffer = Matrix<float>::ones(img_out.shape()) * std::numeric_limits<float>::max();
     img_out *= 0.;
 
     // auto vertices = cam.project(vertices_3d);
-    auto vertices_3d2 = vertices_3d + Vector<float>{0,50,0};
-    fragmentShading(cam.project(vertices_3d), indices, texture, texture_coord_buffer, vertex_z_buffer,z_buffer, img_out);
-    fragmentShading(cam.project(vertices_3d2), indices, texture, texture_coord_buffer, vertex_z_buffer,z_buffer, img_out);
+    // auto vertices_3d2 = vertices_3d + Vector<float>{0,50,0};
+    fragmentShading(cam.project(*vertex_buffer), *vertex_index_buffer, texture, texture_coord_buffer, vertex_z_buffer,z_buffer, img_out);
+    // fragmentShading(cam.project(vertices_3d2), indices, texture, texture_coord_buffer, vertex_z_buffer,z_buffer, img_out);
 
     img_out *= 255.;
 
     imwrite("build/test_img.png", img_out);
-    imwrite("build/zbuffer.png", z_buffer);
+    imwrite("build/zbuffer.png", normalized(z_buffer));
 }
